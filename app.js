f28736963a922bcd1bf9792ff70c66e37fff4b3c
jonmartz <jonmartz91@gmail.com>
2019-05-22 11:39:31 +0300

    Implemented first POST request (register user)

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
 var DButilsAzure = require('./DButils');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
 
 var port = 3000;
 app.listen(port, function () {
@@ -8,7 +12,7 @@ app.listen(port, function () {
 });
 
 app.get('/select', function(req, res){
-    DButilsAzure.execQuery("SELECT * FROM tableName")
+    DButilsAzure.execQuery("SELECT * FROM users")
     .then(function(result){
         res.send(result)
     })
@@ -16,4 +20,25 @@ app.get('/select', function(req, res){
         console.log(err)
         res.send(err)
     })
+});
+
+app.post("/registerUser", (req, res) => {
+    DButilsAzure.execQuery("INSERT INTO users VALUES (" +
+        "'" + req.body.username + "', " +
+        "'" + req.body.password + "', " +
+        "'" + req.body.firstName + "', " +
+        "'" + req.body.lastName + "', " +
+        "'" + req.body.city + "', " +
+        "'" + req.body.country + "', " +
+        "'" + req.body.email + "', " +
+        "'" + req.body.category1 + "', " +
+        "'" + req.body.category2 + "');"
+    )
+        .then(function(result){
+            res.send(result)
+        })
+        .catch(function(err){
+            console.log(err)
+            res.send(err)
+        })
 });
