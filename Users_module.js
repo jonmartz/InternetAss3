2b45bf34c0df7e299e72a1c743d194937a315265
jonmartz <jonmartz91@gmail.com>
2019-05-22 21:44:01 +0300

    Added login

--- Users_module.js
+++ Users_module.js
@@ -1,3 +1,5 @@
+import {Signer as jwt} from "node/base";
+
 var DButilsAzure = require('./DButils');
 
 
@@ -22,6 +24,34 @@ app.post("/registerUser", (req, res) => {
         })
 });
 
+app.post('/login' ,async (req,res)=> {
+    var message= "";
+    var user = await getUser(req.body.username);
+    if(user !== null) {
+        if (user.password !== req.body.password) {
+            message = "wrong password";
+        } else {
+            //create the token.
+            var token = jwt.sign(user, "samplesecret");
+            message = "successful login";
+        }
+    }
+    if (token) {
+        res.status(200).json({message,token});
+    }
+    else{
+        res.status(403).json({message});
+    }
+});
+
+function getUser(username) {
+    return new Promise(async function (resolve, reject) {
+        var user = await DButilsAzure.execQuery('SELECT * FROM users WHERE username='+"'" + username + "'");
+        resolve(user[0]);
+        reject("user not found");
+    });
+}
+
 app.post("/insertQuestion", (req, res) => {
     DButilsAzure.execQuery("INSERT INTO questions VALUES (" +
         "'" + req.body.username + "', " +
@@ -36,4 +66,6 @@ app.post("/insertQuestion", (req, res) => {
             res.send(err)
         })
 });
+
+
 
