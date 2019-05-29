6d082020734a56c0e24abb12e017fa31d2b5d1a7
Shahar Ezra <ezrasha@post.bgu.ac.il>
2019-05-29 18:18:37 +0300

    Update Validator.js

--- Validator.js
+++ Validator.js
@@ -1,4 +1,17 @@
-function validateInjection(req) {
+var countries = "<Countries><Country><ID>1</ID><Name>Australia</Name></Country>" +
+    "<Country><ID>2</ID><Name>Bolivia</Name></Country>" +
+    "<Country><ID>3</ID><Name>China</Name></Country>" +
+    "<Country><ID>4</ID><Name>Denemark</Name></Country>" +
+    "<Country><ID>5</ID><Name>Israel</Name></Country>" +
+    "<Country><ID>6</ID><Name>Latvia</Name></Country>" +
+    "<Country><ID>7</ID><Name>Monaco</Name></Country>" +
+    "<Country><ID>8</ID><Name>August</Name></Country>" +
+    "<Country><ID>9</ID><Name>Norway</Name></Country>" +
+    "<Country><ID>10</ID><Name>Panama</Name></Country>" +
+    "<Country><ID>11</ID><Name>Switzerland</Name></Country>" +
+    "<Country><ID>12</ID><Name>USA</Name></Country></Countries>";
+
+var validateInjection = function validateInjection(req) {
     var forbiddens = ["--", "=", "==", " AND ", " OR ", " and ", " or "];
     for (var key in req.body) {
         for (var str in forbiddens) {
@@ -10,11 +23,29 @@ function validateInjection(req) {
     return true;
 }
 
-function validateName(name) {
+var validateName = function validateName(name) {
     return /^[A-Za-z ]+$/.test(name);
 }
 
-function validateEmail(email) {
+var validateEmail = function validateEmail(email) {
     var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(email);
 }
+
+var validateCountry = function validateCountry(country) {
+    var parser, xmlDoc;
+    parser = new DOMParser();
+    xmlDoc = parser.parseFromString(countries, "text/xml");
+    var arr = [];
+    for (var x in xmlDoc.getElementsByTagName("Name")) {
+        if (!isNaN(x)) {
+            var c = xmlDoc.getElementsByTagName("Name")[x].childNodes[0].nodeValue;
+            arr.push(c);
+        }
+    }
+    if (arr.includes(country))
+        return true;
+    return false;
+}
+
+module.exports = { validateInjection, validateName, validateEmail, validateCountry };
